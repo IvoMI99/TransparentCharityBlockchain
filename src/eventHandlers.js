e688c118e459b52cb7d5e92a7732a8b7d8e6873c
meglenast <stmeglena@gmail.com>
2022-06-11 22:36:16 +0300

    feature: changing page layout

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,14 +1,38 @@
 let btnLogIn = document.querySelector('#btnLogIn');
 let btnSignup = document.querySelector('#btnSignUp');
 
-let contentMain = document.querySelector('#content');
+let content = document.querySelector('#content');
+
+let DONOR = 'Donor';
+let BENEFICIARY = 'Beneficiary';
 
 btnLogIn.addEventListener('click', () => {
-    console.dir('in btn log in');
+
+    let logInChoice = document.querySelector('#logInChoice');
+    if (logInChoice.value == DONOR) {
+        // load layout for donors
+        var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        xhr.open('get', './btn.html', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                content.innerHTML = xhr.responseText;
+            }
+        }
+        xhr.send();
+    } else if (logInChoice.value == BENEFICIARY) {
+        // load layout for beneficiary
+        var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        xhr.open('get', './btn.html', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                content.innerHTML = xhr.responseText;
+            }
+        }
+        xhr.send();
+    }
 });
 
 btnSignup.addEventListener('click', () => {
     console.dir('in btn sign up');
 });
-
 
