3b7ba69c3ff826b7b83e65c755ba2b84b8be5a22
meglenast <stmeglena@gmail.com>
2022-06-11 21:23:44 +0300

    feature: adding styles.css

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,2 +1,14 @@
 let btnLogIn = document.querySelector('#btnLogIn');
 let btnSignup = document.querySelector('#btnSignUp');
+
+let contentMain = document.querySelector('#content');
+
+btnLogIn.addEventListener('click', () => {
+    console.dir('in btn log in');
+});
+
+btnSignup.addEventListener('click', () => {
+    console.dir('in btn sign up');
+});
+
+
