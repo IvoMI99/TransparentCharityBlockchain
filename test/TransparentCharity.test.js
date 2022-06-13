3177190891d524e4623048ab026f4e8c8f4372d0
meglenast <stmeglena@gmail.com>
2022-06-13 14:04:14 +0300

    fix: refactor tests

--- test/TransparentCharity.test.js
+++ test/TransparentCharity.test.js
@@ -3,7 +3,7 @@ const CharityFactory = artifacts.require("CharityFactory");
 
 
 describe('Testing CharityFactory contract', function() {
-    contract('CharityFactory', (accounts) => {
+    contract('CharityFactory', () => {
         before(async () => {
             this.charityFactory = await CharityFactory.deployed();
         })
@@ -15,23 +15,22 @@ describe('Testing CharityFactory contract', function() {
             assert.notEqual(addr, null);
             assert.notEqual(addr, undefined);
         });
-        
+
         it('check empty list of charities', async () => {
             const cnt = await this.charityFactory.cnt();
             const charity = await this.charityFactory.charities(cnt);
             assert.equal(cnt.toNumber(), 0);
             assert.equal(charity, 0x0);
         });
-        
+
         it('add charity to list of charities', async () => {
-            
             let name = "name";
             let desc = "desc";
             let amount = 1;
             let addr = '0x95E7813B58b131A3564Eb1db72A6D17047A4452C';
             let daysOpen = 1;
             let testAccoutn = process.env.TEST_ACCOUNT;
-            
+
             await this.charityFactory.newCharity(name, desc, amount, addr, daysOpen, {from: testAccoutn});
             const charity = await this.charityFactory.charities(0);
             assert.notEqual(charity, 0x0);
